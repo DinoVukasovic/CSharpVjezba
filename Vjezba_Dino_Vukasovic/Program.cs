860d45ed6011bda5f8f1c4bf5fdf600b909ab0ad
LAPTOP-1P438MBR\Zera <dino.vukasovic@yahoo.com>
2023-03-27 09:09:48 +0200

    kreiranje projekta

---
+++ Vjezba_Dino_Vukasovic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba_Dino_Vukasovic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
